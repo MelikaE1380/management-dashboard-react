89b0d5ae00994c698d649a2e4861f4e74710be2c
Melika Eskandarzade <melikaeskandarzadeh9@gmail.com>
2024-12-20 20:47:48 +0330

    rest of sidebar dynamic items

--- src/components/SideRight/SideRight.js
+++ src/components/SideRight/SideRight.js
@@ -1,16 +1,52 @@
+import { useState } from "react";
+import construction from '../ui/svgs/construction.svg';
 
-function SideRight({isOpen,setIsOpen}){
-    return(
+
+function SideRight({ isOpen, setIsOpen }) {
+    const [elements, setElements] = useState([
+        {
+            name: "DASHBOARD",
+            logo: construction
+        },
+        {
+            name: "DASHBOARD",
+            logo: construction
+        },
+        {
+            name: "DASHBOARD",
+            logo: construction
+        }
+    ]);
+
+    return (
         <>
-        <div
-          className={`${
-            isOpen ? 'flex' : 'hidden'
-          } bg-[#F2F2EA] w-[300px] h-full lg:flex`}
-        >
+            <div
+                className={`${isOpen ? 'flex flex-col' : 'hidden'
+                    } bg-[#F2F2EA] w-[300px] border-[#0000ff] border-[1px] border-collapse h-full lg:flex lg:flex-col`}
+            >
+                {/* <div className="grid grid-cols-3 p-2.5 border-collapse">
+                    <div className=" border w-[100%] border-[#b4b4b4] p-2.5 text-2xl text-center">ddd</div>
+                    <div className=" border w-[100%] border-[#b4b4b4] p-2.5 text-2xl text-center">fgg</div>
+                </div> */}
 
-
+                    {elements.map((elem,index) => {
+                    console.log(elem.name);
+                    console.log(elem.logo);
+                   return(
+                    <div key={index} className="flex flex-col border-collapse">
+                    <div className=' flex justify-center items-center gap-[40px] py-[4px] border-[1px] border-[#b5b5b5]'>
+                        <div className="w-1/2 flex justify-center items-center">  <img src={elem.logo} className=' rounded-[100%]' /></div>
+                        <div className=' w-1/2'><p>{elem.name}</p></div>
+                      
+                    </div>
                 </div>
-        
+                   )
+                })}
+
+                
+
+            </div>
+
         </>
     )
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,45 +1,47 @@
 import { useState } from 'react';
 import logo from '../ui/svgs/list.svg';
+import user from '../ui/svgs/user.svg';
 import SideRight from '../SideRight/SideRight';
 
 
 
 function Header() {
 
-const[isOpen,setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-const toggleSideDiv = ()=>{
-    setIsOpen(!isOpen);
-}
+    const toggleSideDiv = () => {
+        setIsOpen(!isOpen);
+    }
 
     return (
         <>
             <header className="bg-[#373737] w-full h-[80px] flex text-white px-[10px] items-center justify-between">
-                <div className="justify-center items-center flex">
-                <img src={logo} onClick={toggleSideDiv}/>
+                <div className="justify-center h-[100%] items-center flex hover:bg-[#242829] p-[8px]">
+                    <img src={logo} onClick={toggleSideDiv} />
                     <div>
                         <p>WebappPro</p>
                     </div>
-                  
-                </div>
-                <div className="flex flex-wrap justify-center items-center gap-[10px]">
 
-                    <div>
+                </div>
+                <div className="flex flex-wrap justify-center items-center gap-[10px] h-[100%]">
+
+                    <div className='hover:bg-[#242829] p-[8px] md:h-[100%] flex justify-center items-center'>
                         <p>HelpSupport</p>
                     </div>
-                    <div>
+                    <div className='hover:bg-[#242829] p-[8px] md:h-[100%] flex justify-center items-center'>
                         <p>Settings</p>
                     </div>
-                    <div>
+                    <div className='flex items-center justify-center p-[8px] gap-[8px] hover:bg-[#242829] md:h-[100%]'>
                         <p>UserName</p>
+                        <div><img src={user} /></div>
                     </div>
                 </div>
             </header>
-            <div className="w-full h-[calc(100vh-80px)] flex">
-            <SideRight isOpen={isOpen} setIsOpen={setIsOpen} />
+            <div className="w-full h-[calc(100vh-80px)] border-[1px]  flex">
+                <SideRight isOpen={isOpen} setIsOpen={setIsOpen} />
                 <div>
 
-                    
+
                 </div>
             </div>
 
